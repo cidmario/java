4bb5e9fa13e4582dd4f7f67dd134296312931465
cidmarioamorim <cidmario.guimaraes@zgsolucoes.com.br>
2020-06-01 23:16:17 -0300

    #6: Validar resposta do usuário caso ele deseje informar um intervalo.

--- src/main/java/exercicios/issue_6/Quadrado.java
+++ src/main/java/exercicios/issue_6/Quadrado.java
@@ -5,27 +5,31 @@ import java.util.Scanner;
 
 public class Quadrado {
 
+	static Scanner s = new Scanner(System.in);
+	static String solicitarIntevalo = "Será impresso o quadrado dos numeros de 15 a 200. \n" +
+			"Deseja informar outro intervalo? (Sim/Não)";
+	static String parada = "Deseja continuar ? (Responda Sim/Não)";
+
 	public static void main(String[] args) {
-		System.out.println();
-			quadrado();
+		quadrado();
 	}
 
 	static void quadrado() {
-		System.out.println("Será impresso o quadrado dos numeros de 15 a 200. \n" +
-				"Deseja informar outro intervalo? (Sim/Não)");
-		Scanner s = new Scanner(System.in);
-		String resp = s.nextLine();
-		System.out.println(resp);
 
-		if (resp.equalsIgnoreCase("sim")) {
-			informaIntervalo();
+		System.out.println(solicitarIntevalo);
+		String resp1 = s.nextLine();
+
+		validarRespostaDoUsuario(resp1, solicitarIntevalo);
+
+		if (resp1.equalsIgnoreCase("sim")) {
+			definirIntevaloPeloUsuario();
 		} else {
-			intevaloDefinidoPeloSistema();
+			definirIntervaloPeloSistema();
 		}
 	}
 
-	static void informaIntervalo() {
-		Scanner s = new Scanner(System.in);
+
+	static void definirIntevaloPeloUsuario() {
 		int inicio = 0, fim = 0, quadrado = 0;
 		System.out.println("Usando numeros 'inteiros', informe o 'inicio' do intervalo:");
 		inicio = s.nextInt();
@@ -37,11 +41,21 @@ public class Quadrado {
 		}
 	}
 
-	static void intevaloDefinidoPeloSistema() {
+	static void definirIntervaloPeloSistema() {
 		int quadrado;
 		for (int i = 15; i <= 200; i++) {
 			quadrado = i * i;
 			System.out.println("O valor de " + i + " ao quadado é >> " + quadrado);
 		}
 	}
+
+	static void validarRespostaDoUsuario(String resp, String mensagem) {
+		//validando resposta do usuário
+		for (int i = 0; (!resp.equalsIgnoreCase("sim")) && (!resp.equalsIgnoreCase("não")); i++) {
+			System.out.println("Resposta Invalida !");
+			System.out.println(mensagem);
+			resp = s.nextLine();
+		}
+	}
 }
+
