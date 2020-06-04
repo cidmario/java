1247af94efac0ab181ad3169a1082cab79ac3a50
cidmarioamorim <cidmario.guimaraes@zgsolucoes.com.br>
2020-06-03 22:45:27 -0300

    #4: Progrma alterado para:     Informar numeros positivos e negativos     Imprimir o numero menor

--- src/main/java/exercicios/issue_4/MaiorMenorNumero.java
+++ src/main/java/exercicios/issue_4/MaiorMenorNumero.java
@@ -4,22 +4,35 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class MaiorMenorNumero {
-	Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		Random gerarNumeros = new Random();
 
 		System.out.println("Quantos numeros serão impressos ? ");
 		int numeros = scanner.nextInt();
-		int maior =0;
-		for(int i=1;i<=numeros;i++){
+		int maior = 0;
+		int menor = 0;
 
-			int numerosAleatorios = gerarNumeros.nextInt(200);
-			System.out.println(numerosAleatorios);
-			if(numerosAleatorios>maior){
+		for (int i = 1; i <= numeros; i++) {
+			int numerosAleatorios = gerarNumeros.nextInt(200) - 50;
+			//Armazena o primeiro numero como maior e menor, pois sera
+			if (i == 1) {
 				maior = numerosAleatorios;
+				menor = numerosAleatorios;
 			}
+			    //Descobre quem é o maior numero
+			if (numerosAleatorios > maior) {
+				maior = numerosAleatorios;
+				//Descrobre o menor numero
+			} else if (numerosAleatorios < menor) {
+				menor = numerosAleatorios;
+			}
+
+			System.out.print(numerosAleatorios);
+			System.out.print(" ");
 		}
-		System.out.println("Maior numero da lista: >>> "+maior);
+		System.out.println("\nMaior numero da lista: >>> " + maior);
+		System.out.println("Menor numero da lista >>> " + menor);
 	}
 }
