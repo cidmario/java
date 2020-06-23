3dc3172e193f5d76744c053a19eb49ca81b9b60f
cidmarioamorim <cidmario.guimaraes@zgsolucoes.com.br>
2020-06-22 22:50:51 -0300

    #7: Criado dois Modelos: Em "DoisMenoresNumeros" tentei fazer utilizando somente o laço for, mas não funcionou 100% Em "DoisMenoresNumeros2" fiz utilizando Array e deu certo. Atendeu os requisitos.

--- src/main/java/exercicios/issue_7/DoisMenoresNumeros.java
+++ src/main/java/exercicios/issue_7/DoisMenoresNumeros.java
@@ -11,20 +11,27 @@ public class DoisMenoresNumeros {
 		System.out.println("Quantos numeros serão impressos ? ");
 		int numeros = scanner.nextInt();
 		int menor = 0;
+		int menor2 = 0;
 
-		for (int i = 1; i <= numeros; i++) {
-			int numerosAleatorios = gerarNumeros.nextInt(200) - 50;
-			//Armazena o primeiro numero como maior e menor, pois sera
-			if(i==1){
-				menor=numerosAleatorios;
-			}
-			//Descobre quem é o menor numero
-			if (numerosAleatorios < menor) {
+		for (int i = 1; i <= numeros; i++) { // determina a quantidade de numeros que será impresso de acordo com a entrada do usuário.
+			int numerosAleatorios = gerarNumeros.nextInt(200) - 50; // Gera aleatoriamente os numeros negativos ou positivos.
+
+			if (i == 1) { //Armazena o primeiro numero como maior e menor, pois sera
 				menor = numerosAleatorios;
+				menor2 = numerosAleatorios;
+			}
+			if (numerosAleatorios < menor) { //Descobre quem é o menor numero a partir de i==2
+				menor2 = menor;
+				menor = numerosAleatorios;
+
+			} else { //Descrobre quém é o segundo menor numero
+				menor = menor2;
+				menor2 = numerosAleatorios;
 			}
 			System.out.println(numerosAleatorios);
 		}
 		System.out.println("\nMenor numero da lista: >>> " + menor);
+		System.out.println("\nO Segundo menor numero da lista: >>> " + menor2);
 	}
 }
 
---
+++ src/main/java/exercicios/issue_7/DoisMenoresNumeros2.java
@@ -0,0 +1,30 @@
+package exercicios.issue_7;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class DoisMenoresNumeros2 {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		Random gerarNumeros = new Random();
+		System.out.println("Quantos numeros serão impressos ? ");
+		int numeros = entrada.nextInt();
+
+		//Array que define a quantidade de numeros que serão criados, de acordo a entrada do usuário
+		int[] numerosAleatorios = new int[numeros];
+
+		//Laço que preenche aleatoriamente os numeros
+		for(int i=0;i<numerosAleatorios.length;i++ ){
+			numerosAleatorios[i] = gerarNumeros.nextInt(200)-50;
+			System.out.println(numerosAleatorios[i]);
+		}
+
+		Arrays.sort(numerosAleatorios); // ordeno do menor para o maior
+
+		System.out.println("Menor: "+numerosAleatorios[0]); // Pego o menor numero
+		System.out.println("Segundo menor: "+numerosAleatorios[1]); //Pego o segundo menor numero
+
+	}
+
+}
